5a11a9441b2f24662f5007299f2af25b4d681b5f
jwala-prasad <jwala.chourey@gmail.com>
2026-08-19 02:51:59 +0530

    main file

--- parent_module/main.tf
+++ parent_module/main.tf
@@ -4,13 +4,13 @@ module "rg" {
 }
 
 module "snts" {
-  source = "../child_module/azurerm_subnet"
-  depends_on = [ module.rg,module.vnts ]
-  snts   = var.snts
+  source     = "../child_module/azurerm_subnet"
+  depends_on = [module.rg, module.vnts]
+  snts       = var.snts
 }
 
 module "vnts" {
-  source = "../child_module/azurerm_vent"
-  depends_on = [ module.rg ]
-  vnts   = var.vnts
+  source     = "../child_module/azurerm_vent"
+  depends_on = [module.rg]
+  vnts       = var.vnts
 }
